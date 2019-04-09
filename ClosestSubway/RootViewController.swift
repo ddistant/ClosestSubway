5fb5fb57b6bd252fa25703f20022b92f3ba84a4d
Daniel <daniel.distant1@gmail.com>
2019-04-09 00:46:22 -0400

    Add settings and initial search functionality with apple maps

--- ClosestSubway/RootViewController.swift
+++ ClosestSubway/RootViewController.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 class RootViewController: UIViewController {
+    // MARK: - enums
+    enum Constants {
+        static let title = "Closest Subways"
+        static let cellIdentifier = "cellIdentifier"
+        
+        enum LocationAlert {
+            static let title = "Location Services Not Allowed"
+            static let message = "Location access is restricted. To change this, go to Settings > Privacy > Location Services and allow location access."
+            static let cancel = "Cancel"
+            static let settings = "Go to Settings"
+        }
+    }
     
     // MARK: - IBOutlets
     @IBOutlet weak var tableView: UITableView!
@@ -20,15 +32,55 @@ class RootViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = Constants.title
         setupTableView()
-        title = "Closest Subways"
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+//        checkLocationAccessEnabled()
     }
     
     // MARK: - private methods
     private func setupTableView() {
         tableView.dataSource = self
         tableView.delegate = self
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellIdentifier")
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: Constants.cellIdentifier)
+    }
+    
+    private func checkLocationAccessEnabled() {
+        if viewModel.locationServicesEnabled == false {
+            showUserDeniedLocationAlert()
+        }
+    }
+    
+    private func showUserDeniedLocationAlert() {
+        let alertController = UIAlertController(title: Constants.LocationAlert.title,
+                                                message: Constants.LocationAlert.message,
+                                                preferredStyle: .alert)
+        
+        let cancel = UIAlertAction(title: Constants.LocationAlert.cancel, style: .cancel)
+        let goToSettings = UIAlertAction(title: Constants.LocationAlert.settings, style: .default) { [weak self] _ in
+            self?.deeplinkToSettings()
+        }
+        
+        alertController.addAction(cancel)
+        alertController.addAction(goToSettings)
+        
+        present(alertController, animated: true)
+    }
+    
+    private func deeplinkToSettings() {
+        guard let bundleId = Bundle.main.bundleIdentifier,
+            let appSettingsUrl = URL(string: UIApplication.openSettingsURLString + bundleId) else {
+            print("📌 could not deeplink to settings")
+            return
+        }
+        
+        if UIApplication.shared.canOpenURL(appSettingsUrl) {
+            UIApplication.shared.open(appSettingsUrl, completionHandler: { (success) in
+                print("📉 Settings opened: \(success)")
+            })
+        }
     }
 }
 
@@ -44,7 +96,7 @@ extension RootViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cellIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellIdentifier, for: indexPath)
         
         cell.textLabel?.text = viewModel.subwayStations[indexPath.row].name
         return cell
--- ClosestSubway/RootViewModel.swift
+++ ClosestSubway/RootViewModel.swift
@@ -11,71 +11,129 @@ import MapKit
 
 struct SubwayStation {
     let name: String
+    let address: String
 }
 
 class RootViewModel: NSObject {
+    // MARK: - enums
+    private enum Constants {
+        static let query = "subway"
+    }
     
     // MARK: - properties
     private (set) var subwayStations = [SubwayStation]()
     private (set) var currentLocation: CLLocation?
-    
-    private var searchCompleter: MKLocalSearchCompleter {
-        let completer = MKLocalSearchCompleter()
-        completer.delegate = self
-        
-        return completer
-    }
-    
-    private var locationManager: CLLocationManager {
-        let manager = CLLocationManager()
-        manager.delegate = self
-        manager.desiredAccuracy = kCLLocationAccuracyBest
-        manager.requestAlwaysAuthorization()
-        
-        if CLLocationManager.locationServicesEnabled() {
-            manager.startUpdatingLocation()
+    private (set) var locationServicesEnabled = false {
+        didSet {
+            if locationServicesEnabled == true && CLLocationManager.locationServicesEnabled() {
+                locationManager?.startUpdatingLocation()
+            }
         }
-        
-        return manager
     }
+    
+    private var locationManager: CLLocationManager?
     
     // MARK: - initializer
     override init() {
         super.init()
         
-        getClosestSubwayStations()
+        setupLocationManager()
+        startSubwaySearch()
     }
     
-    // MARK: - public methods
-    func getCurrentLocation() {
+    // MARK: - public methods    
+    func startSubwaySearch() {
+        // clear current subways
+        subwayStations = []
         
-    }
-    
-    func getClosestSubwayStations() {
-        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        // create request
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = Constants.query
+        request.region = makeRegion(from: currentLocation ?? CLLocation())
         
-        searchCompleter.queryFragment = "subway"
-        
-        defer {
-            UIApplication.shared.isNetworkActivityIndicatorVisible = false
+        // create completion handler
+        let completionHandler: MKLocalSearch.CompletionHandler = { [weak self] (response, error) in
+            guard error == nil else {
+                // implement real error handling
+                print("✂️ search error:", error?.localizedDescription ?? "error description failed")
+                return
+            }
+            
+           self?.createSubwayStations(from: response)
         }
         
-        let mockStation1 = SubwayStation(name: "station1")
-        let mockStation2 = SubwayStation(name: "station2")
-        let mockStation3 = SubwayStation(name: "station3")
+        // create and start search
+        let localSearch = MKLocalSearch(request: request)
+        localSearch.start(completionHandler: completionHandler)
+    }
+    
+    // MARK: - private methods
+    private func setupLocationManager() {
+        let manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
         
-        subwayStations = [mockStation1, mockStation2, mockStation3]
+        if CLLocationManager.locationServicesEnabled() {
+            manager.startUpdatingLocation()
+        }
+        
+        locationManager = manager
+    }
+    
+    private func createSubwayStations(from response: MKLocalSearch.Response?) {
+        guard let response = response else {
+            print("🥶 no response present")
+            return
+        }
+        
+        print("💀 response:", response)
+        
+        response.mapItems.forEach {
+            let station = SubwayStation(name: $0.name ?? "no name found",
+                                        address: ($0.placemark.thoroughfare ?? "" ) + ($0.placemark.subThoroughfare ?? ""))
+            
+            subwayStations.append(station)
+        }
+    }
+    
+    private func makeRegion(from location: CLLocation) -> MKCoordinateRegion {
+        let span = MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)
+        let region = MKCoordinateRegion(center: location.coordinate, span: span)
+        
+        return region
     }
 }
 
-// MARK: - MKLocalSearchCompleterDelegate
-extension RootViewModel: MKLocalSearchCompleterDelegate {
-    
-}
+
 
 // MARK: - CLLocationManagerDelegate
 extension RootViewModel: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .denied, .restricted:
+            locationServicesEnabled = false
+        default:
+            locationServicesEnabled = true
+        }
+        
+        print("🧠 authorization status:", status.rawValue)
+    }
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         currentLocation = locations.last
+    }
+}
+
+class SettingsManager {
+    // MARK: - enums
+    enum SettingsBundleKeys {
+        static let location = "location_enabled_preference"
+    }
+    
+    class func checkAndExecuteSettings() {
+        if UserDefaults.standard.bool(forKey: SettingsBundleKeys.location) {
+           // WIP, do something here
+        }
     }
 }
